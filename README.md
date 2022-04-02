daa78e0df36d3b0e3c242a13149f3c3008371023
rlw92 <r_l_williams@yahoo.com>
2022-04-02 10:38:35 +0100

    Bit of a mess trying to figure out the decider function to announce the winner. Will have to come back to ti and either tackle it from a new angle or understand why its not working.

--- README.md
+++ README.md
@@ -8,3 +8,21 @@ would like to be able to tell which player it is by having a trasparent colored 
 When the marker is down the marker should be in the players color.
 
 *The congratulations message should be comepletely in the players color.
+
+
+pseudocode for players
+
+when player clicks on a square:
+the index of the div gets put iinto that players choice array.
+The players choice array is then compared to each winning array to see if it  matches
+if it matches one of the winning arrays then the game is stopped, a message appears congratulating the player
+and the game resets
+
+pseudocode for winning hand 
+
+every time  a player clicks a box his index is  entered into his array and his array is then  put into a function
+that loops through each winning array, if a number matches then it put into the winning hand array and if 
+there are three matches (winningHand.length === 3) then the player wins and if not then the game continues with the next player.
+
+
+
--- js.js
+++ js.js
@@ -8,9 +8,66 @@
  // array of players information
  let playerBase = [];
  let gameBoard=["","","","","","","","",""]
-  let mip;
+ //player in play
  let pip;
+ //chosen color, currently only blue or red
  let col;
+ //winning arrays:
+let winArr = [
+  /*mock to test*/[1,2,3],
+[0,3,6],
+[0,4,8],
+[0,1,2],
+[1,4,7],
+[2,5,8],
+[2,4,6],
+[2,5,8],
+[3,4,5],
+[6,7,8]
+]
+ //test 
+ let arr1 = [1,2,3];
+ let arr2 = [1,2,3];
+ 
+ /* practice code to be deleted when feature has been built
+ const decider = () => {
+   let arr3 = [];
+ for (i=0;i<arr1.length;i++){
+   arr1[i];
+
+   for(d=0;d<arr2.length;d++){
+
+   if(arr1[i]===arr2[d]){
+     arr3.push(arr1[i])}
+     else {console.log("no winning match");}
+   }
+   if(arr3.length===3){alert("winning match")}
+   
+   }
+ 
+  }
+  */
+
+  //decider function 
+  const decide = () => {
+    
+  for (i=0;i<arr1.length;i++){
+    let arr3 = [];
+    arr1[i];
+
+   
+    for(d=0;d<winArr.length;d++){
+      winArr[d];
+      
+     for(z=0;z<winArr[d].length;z++){       
+      
+      if(arr1[i]===winArr[d][z]){
+      arr3.push(arr1[i])}
+      else {console.log("no match");}
+      console.log(arr3);
+    
+
+    }}if(arr3.length===3){alert("winning match")}}}  
  
 
 //my first module. it controls the initial forms at the start
@@ -75,10 +132,10 @@ for (let i = 0; i < fields.length; i++) {
                                         fields[i].textContent = pip.marker;
                                          fields[i].style.color = col;
                                          let f = fields[i].dataset.index
-                                         gameBoard.splice(+f, 1, +f);
                                          pip.arr.push(+f);
                                          playRound.chan();
                                          //added below log just to check theyre being added
+                                         gameBoard.splice(+f, 1, +f);
                                          console.log(gameBoard);
                                         });
 }
@@ -90,15 +147,3 @@ return {selectbox, chan}
  
 playRound.selectbox();
 
-//winning arrays:
-let winArr = [
-[0,3,6],
-[0,4,8],
-[0,1,2],
-[1,4,7],
-[2,5,8],
-[2,4,6],
-[2,5,8],
-[3,4,5],
-[6,7,8]
-]
